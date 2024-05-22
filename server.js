0e6f9cfaa88126aab94d2207790c30c38bfe02dd
VibhaGupta8102 <vibhagupta81028@gmail.com>
2024-05-23 01:52:23 +0530

    updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoute.js'
 import  cors  from "cors";
 import categoryRoutes  from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-const path= require('path')
+import path from 'path';
 
 //configure env
 dotenv.config();
