199760551fd0598045da6fbf31cd2bdca7f57060
VibhaGupta8102 <vibhagupta81028@gmail.com>
2024-05-23 02:43:29 +0530

    ek change aur karo

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //static file
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname , './client/build/index.html'))
+    res.sendFile(path.join(__dirname , 'client/build/index.html'))
 })
 
 //PORT
