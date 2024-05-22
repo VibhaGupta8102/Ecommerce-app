e8e4def794b9f7a04b1c37eadcf109cb369cc390
VibhaGupta8102 <vibhagupta81028@gmail.com>
2024-05-23 02:53:14 +0530

    commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname , 'client/build/index.html'))
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
 
 //PORT
