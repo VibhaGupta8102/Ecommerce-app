b679698874de90dd90e7e081f6bb9aaeb446827d
VibhaGupta8102 <vibhagupta81028@gmail.com>
2024-05-24 23:35:04 +0530

    commit

--- server.js
+++ server.js
@@ -24,16 +24,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
-//routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/category', categoryRoutes)
-app.use('/api/v1/product', productRoutes)
-
-//rest api 
-app.get('/',(req,res)=>{
-    res.send("<HomePage />")
-})
-
  // Define __dirname for ES modules
  const  __filename = fileURLToPath(import.meta.url);
  const  __dirname = path.dirname(__filename);
@@ -43,7 +33,20 @@ app.get('/',(req,res)=>{
   app.use(express.static(path.join(__dirname, '/client/build')))
 
 //  //render client
-  app.get('*', (req,res)=>     res.sendFile(path.join(__dirname, '/client/build/index.html')))
+  // app.get('*', (req,res)=>     res.sendFile(path.join(__dirname, '/client/build/index.html')))
+
+  //rest api 
+app.get('/',(req,res)=>{
+  res.send("<h1>Welcome to ecommerce app</h1>")
+})
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes)
+app.use('/api/v1/product', productRoutes)
+
+//  //render client
+app.get('*', (req,res)=>     res.sendFile(path.join(__dirname, '/client/build/index.html')))
 
 //PORT
 const PORT= process.env.PORT || 8080;
